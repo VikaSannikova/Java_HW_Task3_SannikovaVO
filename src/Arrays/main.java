c2912ab048ef36d2ba98777f0cd7d1907a1fe44e
VikaSannikova <vikasannikova1998@gmail.com>
2017-10-25 23:09:23 +0300

    arrays smth done

--- src/Arrays/main.java
+++ src/Arrays/main.java
@@ -2,6 +2,8 @@ package Arrays;
 
 import java.util.Arrays;
 
+import static java.lang.Math.random;
+
 public class main {
     public static void main(String[] args) {
     //task a
@@ -17,5 +19,33 @@ public class main {
         System.out.print("]");
         System.out.println();
     //task b
+        int[] arr2 = new int[20];
+        int evenCount=0;
+        for(int i=0;i<arr2.length;i++){
+            arr2[i] = (int)(Math.random()*11);
+            if (arr2[i]%2==0) evenCount++;
+        }
+        System.out.println("arr2: "+Arrays.toString(arr2));
+        System.out.println("Amount of evev numbers = "+evenCount
+                +"; Amount of odd numbers = " + (arr2.length-evenCount) );
+        System.out.println();
+    //task c
+        int[] arr3 = new int[10];
+        for(int i=0;i<arr3.length;i++){
+            arr3[i] = (int)(Math.random()*101);
+        }
+        System.out.println("arr3: "+Arrays.toString(arr3));
+        for(int i=1;i<arr3.length;i+=2){
+            arr3[i]=0;
+        }
+        System.out.println("modified arr3: "+Arrays.toString(arr3));
+        System.out.println();
+        
+    //task d
+        int[] arr4 = new int[15];
+        for(int i=0;i<arr4.length;i++){
+            arr4[i] = (int)(Math.random()*101-50);
+        }
+        System.out.println(Arrays.toString(arr4));
     }
 }
